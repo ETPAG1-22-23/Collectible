bdc41038dfbe7a0120abe6f049114dc5e392b78c
99aa66 <emanuelecirronis123@gmail.com>
2023-01-27 19:51:19 +0100

    Code_Potion_Utilisable_Manuellement
    Un objet que quand le joueur est dessus si il appuie sur le Bouton E tu peut l'utiliser directement.

--- Assets/Collectible_Ramassable.cs
+++ Assets/Collectible_Ramassable.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Collectible_Ramassable : MonoBehaviour
 {
+    private int NombreDePick = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +17,21 @@ public class Collectible_Ramassable : MonoBehaviour
         
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    void PickUpPotion(Collider2D collision)
     {
-        Debug.Log("BEH");
-        if (collision.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+        if (NombreDePick == 1)
         {
-            collision.gameObject.GetComponentInChildren<Health>().pv += 10;
-            Destroy(gameObject);
-            Debug.Log("BEHHHHHHHHHHHH");
+            if (collision.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E))
+            {
+                collision.gameObject.GetComponentInChildren<Health>().pv += 10;
+                Destroy(gameObject);
+                Debug.Log("BEHHHHHHHHHHHH");
+                NombreDePick--;
+            }
         }
+    }
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        PickUpPotion(collision);
     }
 }
